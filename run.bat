2822d2c7f022a1f8e5255cfc6b26362f8eb108de
sebastianneuner <228686635+sebastianneuner@users.noreply.github.com>
2026-05-02 18:54:41 +0200

    fixed run and setup bat

--- run.bat
+++ run.bat
@@ -1,15 +1,3 @@
 @echo off
-setlocal
-
-if exist .venv\Scripts\python.exe (
-    .\.venv\Scripts\python.exe game2d.py
-    exit /b %errorlevel%
-)
-
-if exist venv\Scripts\python.exe (
-    .\venv\Scripts\python.exe game2d.py
-    exit /b %errorlevel%
-)
-
-echo Fehler: Keine virtuelle Umgebung gefunden. Fuehre zuerst setup_venv.bat aus.
-exit /b 1
+call venv\Scripts\activate.bat
+python game2d.py
